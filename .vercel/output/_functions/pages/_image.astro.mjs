8194b1ce65a99b5810cb42781ef2ca6097b052a6
patleserdev <leserpat@hotmail.com>
2026-02-21 09:38:10 +0100

    update

--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { p as page } from '../chunks/generic_BUC1uCOS.mjs';
-export { renderers } from '../renderers.mjs';
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import vercel from "@astrojs/vercel"; // classique, pas serverless
+  import vercel from "@astrojs/vercel"; // classique, pas serverless
 
-export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-
-  output: "server",
-  adapter: vercel({
-    imageService: true,
-    webAnalytics: {
-      enabled: true,
+  export default defineConfig({
+    vite: {
+      plugins: [tailwindcss()]
     },
-  }),
-});
+
+    output: "server",
+    adapter: vercel({
+      imageService: true,
+      webAnalytics: {
+        enabled: true,
+      },
+    }),
+  });
